87a7534547aa0be93922162ca7faf56eba7a8321
Parth Oza <plusparth@gmail.com>
2016-06-05 14:41:01 -0400

    Changed sensor.ino to wait for serial input to send data

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -4,68 +4,112 @@
 #include <utility/imumaths.h>
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-const int currentSensorPin=0;
-const int UVSensorPin=0;
+const int CURRENT_SENSOR_PIN = 0;
+const int UV_SENSOR_PIN = 0;
+const int DUST_SENSOR_PIN = 0;
+
+const int GYRO = 0;
+const int ACCEL = 1;
+const int MAGNET = 2;
+const int EULERVEC = 3;
+const int LINACCEL = 4;
+const int GRAVITY = 5;
+const int CURRENT = 6;
+const int UV = 7;
+const int DUST = 8;
+const int TEMP = 9;
+
+
+int whatToSend;
+bool bnoFound = true;
 
 void setup(){
-  Serial.begin(28800);
-  if (!bno.begin()) {
-    Serial.println("HELP, SOMETHING HAPPENED");
-  }
-  pinMode(currentSensorPin,INPUT);
-  pinMode(UVSensorPin,INPUT);
-}
-void loop() {
-  Serial.print("Current:");
-  Serial.println(analogRead(currentSensorPin));
-  Serial.print("UV:");
-  Serial.println(analogRead(UVSensorPin));
-  
-  // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> vec;
-  vec = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  Serial.println("Gyro:");
-  Serial.print(vec.x());
-  Serial.print(",");
-  Serial.print(vec.y());
-  Serial.print(",");
-  Serial.print(vec.z());
-  Serial.println();
-  
-  vec = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  Serial.println("Accel:");
-  Serial.print(vec.x());
-  Serial.print(",");
-  Serial.print(vec.y());
-  Serial.print(",");
-  Serial.print(vec.z());
-  Serial.println();
-  
-  vec = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  Serial.println("Magnet:");
-  Serial.print(vec.x());
-  Serial.print(",");
-  Serial.print(vec.y());
-  Serial.print(",");
-  Serial.print(vec.z());
-  Serial.println();
-  
-  vec = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  Serial.println("Direction:");
-  Serial.print(vec.x());
-  Serial.print(",");
-  Serial.print(vec.y());
-  Serial.print(",");
-  Serial.print(vec.z());
-  Serial.println();
-  
-  
-  delay(2000);
+    Serial.begin(28800);
+    if (!bno.begin()) {
+        Serial.println("HELP, SOMETHING HAPPENED");
+        bnoFound = false;
+    }
+    pinMode(CURRENT_SENSOR_PIN, INPUT);
+    pinMode(UV_SENSOR_PIN, INPUT);
 }
 
+void writeVector(imu::Vector<3> vec) {
+    Serial.print(vec.x());
+    Serial.print(",");
+    Serial.print(vec.y());
+    Serial.print(",");
+    Serial.print(vec.z());
+    Serial.println();
+}
+
+void writeAnalogPin(int pin) {
+    Serial.println(analogRead(pin));
+}
+
+void loop() {
+    while(Serial.available() == 0);
+    whatToSend = Serial.parseInt();
+    switch(whatToSend) {
+        case GYRO:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case ACCEL:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case MAGNET:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case EULERVEC:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_EULER));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case LINACCEL:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case GRAVITY:
+            if(bnoFound) {
+                writeVector(bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY));
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case TEMP:
+            if(bnoFound) {
+                Serial.println(bno.getTemp());
+            } else {
+                Serial.println("BNO055 NOT FOUND");
+            }
+            break;
+        case CURRENT:
+            writeAnalogPin(CURRENT_SENSOR_PIN);
+            break;
+        case UV:
+            writeAnalogPin(UV_SENSOR_PIN);
+            break;
+        case DUST:
+            writeAnalogPin(DUST_SENSOR_PIN);
+            break;
+        default:
+            Serial.println("INVALID");
+            break;
+    }
+}
